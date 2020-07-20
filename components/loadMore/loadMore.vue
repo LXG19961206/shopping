70b203ed42537833a6b7721d53f07614aeb96bc1
lxg1996 <lxg1996@192.168.0.100>
2020-07-20 18:49:41 +0800

    remix base

--- components/loadMore/loadMore.vue
+++ components/loadMore/loadMore.vue
@@ -1,15 +1,15 @@
 <template>
 	<view 
-		class="lxg-ui-loadMore-container"
+		class="lxg-ui-loadMore-container" 
 		@touchstart="touchStart($event)">
 		<text 
-			class="lxg-ui-loadMore-text"
-			v-show="isLoading == false"
+			class="lxg-ui-loadMore-text" 
+			v-show="isLoading == false" 
 			v-text="tipsText">
 		</text>
-		<image
-			class="lxg-ui-loadMore-icon"
-			v-show="isLoading"
+		<image 
+			class="lxg-ui-loadMore-icon" 
+			v-show="isLoading" 
 			src="../../static/loading.gif" 
 			mode="">
 		</image>
@@ -17,92 +17,94 @@
 </template>
 <script>
 	export default {
-		props:{
-			currentPage:{
+		props: {
+			currentPage: {
 				default: 1,
-				type:Number
+				type: Number
 			},
-			pageSize:{
+			pageSize: {
 				default: 5,
-				type:Number
+				type: Number
 			},
-			getMore:{
-				type:Function
+			getMore: {
+				type: Function
 			},
-			canLoadMore:{
-				type:Boolean,
-				default:true
+			canLoadMore: {
+				type: Boolean,
+				default: true
 			}
 		},
-		data(){
+		data() {
 			return {
-				tipsText:'上拉加载更多...',
-				pageHeight:'',
-				pageY:'',
-				isLoading:false,
-				timer:true
+				tipsText: '上拉加载更多...',
+				pageHeight: '',
+				pageY: '',
+				isLoading: false,
+				timer: true
 			}
 		},
-		computed:{
-			pageTemp(){
+		computed: {
+			pageTemp() {
 				return this.pageHeight - this.pageY
 			}
 		},
-		methods:{
-			touchStart(e){
+		methods: {
+			touchStart(e) {
 				console.log(e)
 			}
 		},
 		mounted() {
-			const that = this 
-			this.$nextTick(()=>{
+			const that = this
+			this.$nextTick(() => {
 				const content = document.querySelector('.content')
-				content.addEventListener('touchstart',(e)=>{
+				content.addEventListener('touchstart', (e) => {
 					this.isLoading = false
 					// 获取我们点击屏幕的点
 					this.pageY = e.targetTouches[0].pageY
 					// 获取当前一屏幕的高度
 					// 最终的结果还要减去50是因为tabBar的高度不应该计算在内
 					this.pageHeight = document.querySelector('html').scrollHeight - 50
-					if(this.pageTemp < 50){
+					if (this.pageTemp < 50) {
 						// 如果我们点击的位置是屏幕的下边沿50px之内的话
-						console.log('距离页面的底部有'+(this.pageTemp) + 'px') 
+						console.log('距离页面的底部有' + (this.pageTemp) + 'px')
 					}
 				})
 				// 根据touchmove事件,配合刚才的touchstart事件,我们可以判断屏幕是否是被上拉或是下拉
-				content.addEventListener('touchmove',(e)=>{
+				content.addEventListener('touchmove', (e) => {
 					// 当父组件通知我们已经不能进行下拉加载时,阻止方法执行,并且修改提示文字
-					if(this.canLoadMore == false){
+					if (this.canLoadMore == false) {
 						this.tipsText = '已经到最底啦...'
-						return 
+						return
 					}
-					if(this.pageTemp>50) return  // 如果距离大于50,那就不触发上拉加载功能
-					const { pageY } = e.targetTouches[0]
-					console.log(pageY,this.pageY)
+					if (this.pageTemp > 50) return // 如果距离大于50,那就不触发上拉加载功能
+					const {
+						pageY
+					} = e.targetTouches[0]
+					console.log(pageY, this.pageY)
 					// 如果判断是下拉,直接 return ,不执行方法
-					if(pageY > this.pageY) return
+					if (pageY > this.pageY) return
 					this.isLoading = true
 					// 需要加入节流功能,不然touchmove事件会一直触发
-					if(this.timer==false) return 
+					if (this.timer == false) return
 					this.timer = false
-					setTimeout(()=>{
-						return new Promise((resolve,reject)=>{
-							try{
+					setTimeout(() => {
+						return new Promise((resolve, reject) => {
+							try {
 								this.getMore()
 								resolve()
-							}catch(err){
+							} catch (err) {
 								console.log(new Error(err))
 								reject()
 							}
-						}).then(res=>{
-							setTimeout(()=>{
+						}).then(res => {
+							setTimeout(() => {
 								this.isLoading = false
 								this.timer = true
-							},1000)
-						}).catch(err=>{
+							}, 1000)
+						}).catch(err => {
 							console.log(new Error(err))
 						})
-					},2000)
+					}, 2000)
 				})
 			})
 		}
@@ -110,17 +112,20 @@
 </script>
 
 <style lang="scss">
-@import "../../app.scss";
-.lxg-ui-loadMore-container{
-	text-align: center;
-	.lxg-ui-loadMore-text {
-		color: #aaa;
-		font-size: 21rpx;
+	@import "../../app.scss";
+
+	.lxg-ui-loadMore-container {
+		text-align: center;
+
+		.lxg-ui-loadMore-text {
+			color: #aaa;
+			font-size: 21rpx;
+		}
+
+		.lxg-ui-loadMore-icon {
+			height: 60rpx;
+			width: 80rpx;
+			margin-top: 10rpx;
+		}
 	}
-	.lxg-ui-loadMore-icon {
-		height: 60rpx;
-		width: 80rpx;
-		margin-top: 10rpx;
-	}
-}
 </style>
